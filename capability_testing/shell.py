ad9b4736791cb4c449af74c32c604b1d6967d633
jaredmichaelsmith <jared@jaredsmith.io>
2019-01-03 15:41:19 -0500

    Fix tylers horrible spacing

--- capability_testing/shell.py
+++ capability_testing/shell.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
+
+
 import subprocess
+
+
 def main():
     try:
-      subprocess.run(["python","--version"], shell=True, check=True)
+        subprocess.run(["python","--version"], shell=True, check=True)
     except Exception:
-      return False
+        return False
     return True
+
+
 if __name__ == '__main__':
     main()
-    
