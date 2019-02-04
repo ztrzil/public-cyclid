945bd334098cee143da50f9513bf6bc018de7c5f
Tyler McDaniel <bmcdan16@utk.edu>
2019-02-04 09:41:49 -0500

    Push ethereum script

---
+++ capability_testing/ethereum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+from web3.auto.infura import w3
+
+
+def main():
+    del os.environ['INFURA_API_KEY']
+    os.environ['INFURA_API_KEY'] = "fcd622f279b746a7bceed92fa22973ee"
+    print(w3.eth.getBlock('latest'))
+    return w3.isConnected()
+
+
+if __name__ == '__main__':
+    print(main())
