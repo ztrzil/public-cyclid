c92125fd0bb7af2c1e050b9e23da6a5543993390
Zac <ztrzil@vols.utk.edu>
2019-02-05 17:09:46 -0500

    more shell stuff

--- capability_testing/shell.py
+++ capability_testing/shell.py
@@ -5,9 +5,11 @@ import subprocess
 
 
 def main():
+    p = subprocess.Popen("python", shell=True)
     try:
-        subprocess.run(["python","--version", "exit()"], shell=True, check=True)
-        #p = subprocess.Popen("python", shell=True, check=True)
+        #subprocess.run(["python","--version"], shell=True, check=True)
+        out, err = p.communicate('--version', timeout=10)
+        out, err = p.communicate('exit()', timeout=10)
     except Exception:
         return False
     return True
