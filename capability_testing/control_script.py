ec47c806fe42735df9fe1907076eb80a61295dc4
Zac <ztrzil@vols.utk.edu>
2019-02-05 11:05:42 -0500

    control script

---
+++ capability_testing/control_script.py
@@ -0,0 +1,103 @@
+import json
+
+json_file = 'resources/service_restrictions.json'
+scripts = ['tcp.py', 'udp.py', 'raw_sockets.py', 'shell.py', 'external.py',
+'httpserver_80.py', 'httpserver_8000.py', 'mail.py', '', '', '', 'socks.py',
+'']
+
+def read_file(filename):
+  with open(json_file, 'r') as fp:
+    d = json.load(fp)
+  return d
+
+def get_entry(d, uuid):
+  for entry in d[1:]:
+    if entry['UUID'] == uuid:
+      print(entry)
+      return entry
+  return None
+
+def run_scripts(entry):
+  if entry['TCP'] != 'N':
+    print('Checking TCP...')
+    # call script with subproc.
+  else:
+    print('Skipping TCP')
+  if entry['UDP'] != 'N':
+    print('Checking UDP...')
+    # call script with subproc.
+  else:
+    print('Skipping UDP')
+  if entry['Raw Sockets/Forge Packets'] != 'N':
+    print('Checking Raw Sockets/Forging Packets...')
+    # call script with subproc.
+  else:
+    print('Skipping Raw Sockets/Forging Packets')
+  if entry['Output of `whoami`/`id`'] != 'N':
+    print('Checking access to shell...')
+    # call script with subproc.
+  else:
+    print('Skipping test for access to shell')
+  if entry['Externally Reachable'] != 'N':
+    print('Checking if externally reachable...')
+    # call script with subproc.
+  else:
+    print('Skipping check for external reachability')
+  if entry['Host a Server'] != 'N':
+    print('Checking ability to host a server on port 80...')
+    # call script with subproc.
+    print('Checking ability to host a server on port 8000...')
+    # call script with subproc.
+  else:
+    print('Skipping test to host a server')
+  if entry['Send Email'] != 'N':
+    print('Checking if email can be sent...')
+    # call script with subproc.
+  else:
+    print('Skipping test to see if email can be sent')
+  if entry['Seed Bittorrent File'] != 'N':
+    print('Checking if torrent can be seeded...')
+    # call script with subproc.
+  else:
+    print('Skipping test to see if torrent can be seeded')
+  if entry['Reach BTC P2P Network'] != 'N':
+    print('Attempting to reach BTC P2P network...')
+    # call script with subproc.
+  else:
+    print('Skipping test to reach BTC P2P network')
+  if entry['Reach ETH P2P Network'] != 'N':
+    print('Attempting to reach ETH P2P network...')
+    # call script with subproc.
+  else:
+    print('Skipping test to reach ETH P2P network')
+  if entry['Run SOCKS Proxy'] != 'N':
+    print('Attempting to run SOCKS Proxy...')
+    # call script with subproc.
+  else:
+    print('Skipping test to run SOCKS Proxy')
+    # call script with subproc.
+  if entry['Scanning'] != 'N':
+    pass
+
+
+def select_scripts(d):
+  uuid = input('Input the UUID of the service being tested: ')
+  try:
+    int(uuid)
+  except:
+    print('Expected integer input corresponding to the UUID of the service \
+    being tested. Refer to the Cyclid Google Sheet for a mapping of UUIDs to \
+    service')
+    return
+  entry = get_entry(d, uuid)
+  if entry == None:
+    print('Failed to find entry for the entered UUID. Refer to the Cyclid \
+    Google Sheet for a mapping of UUIDs to service.')
+    return
+
+  run_scripts(entry)
+
+
+
+d = read_file(json_file)
+select_scripts(d)
