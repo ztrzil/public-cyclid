c15378531d02572daeeb719662d7527d50cf3577
Zac <ztrzil@vols.utk.edu>
2019-02-05 17:05:11 -0500

    exit shell automatically

--- capability_testing/shell.py
+++ capability_testing/shell.py
@@ -6,8 +6,8 @@ import subprocess
 
 def main():
     try:
-        #subprocess.run(["python","--version"], shell=True, check=True)
-        subprocess.run(["bash"], shell=True, check=True)
+        subprocess.run(["python","--version", "exit()"], shell=True, check=True)
+        #p = subprocess.Popen("python", shell=True, check=True)
     except Exception:
         return False
     return True
