ba8f63c494163114a88846f9c42310c1da1ca008
Zac <ztrzil@vols.utk.edu>
2019-02-07 08:31:38 -0500

    update http server test on port 8000

--- capability_testing/httpserver_8000.py
+++ capability_testing/httpserver_8000.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
 
-
 import subprocess
+import time
+import urllib.request
 
 
 def main():
+    rv = False
+    contents = None
+
     try:
-        subprocess.run(["python", "-m", "http.server", "8000"], shell=True, check=True)
-    except Exception:
-        return False
-    return True
+        p = subprocess.Popen("python -m http.server 8000", shell=True)
+        time.sleep(1)
+        contents = urllib.request.urlopen('http://0.0.0.0:8000').read()
+        print(contents)
+        out, err = p.communicate(timeout=3)
+        print(out, err) # this line should never print
+    except subprocess.TimeoutExpired:
+        if contents != None:
+            rv = True
+    except Exception as e:
+        print(e)
+    finally:
+      p.kill()
+    return rv 
 
 
 if __name__ == '__main__':
